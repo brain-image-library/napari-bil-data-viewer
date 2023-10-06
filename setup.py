2210ce7583b0f6ee25a8393c5ce83e353012f93b
Iana Vasylieva <iana.vasylieva@gmail.com>
2023-10-06 15:55:18 -0400

    Fix missing images when installed from pypi

--- setup.py
+++ setup.py
@@ -5,4 +5,7 @@ from setuptools import setup
 
 # https://github.com/pypa/setuptools_scm
 use_scm = {"write_to": "napari_bil_data_viewer/_version.py"}
-setup(use_scm_version=use_scm)
+setup(
+    use_scm_version=use_scm,
+    include_package_data=True
+)
